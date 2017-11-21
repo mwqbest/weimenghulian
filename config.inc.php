546332e89672608d6882ad736a9c80e5b25ba2b8
mwqbest <sd_mwq@163.com>
2017-11-21 11:14:13 +0800

    add

---
+++ config.inc.php
@@ -0,0 +1,13 @@
+<?php
+return array (
+    'DB_TYPE'=>'mysql',
+	'DB_HOST'=>'localhost',
+	'DB_NAME'=>'weimenghulian',
+	'DB_USER'=>'root',
+	'DB_PWD'=>'',
+	'DB_PORT'=>'3306',
+	'DB_PREFIX'=>'weimenghulian_',
+	'DB_CHARSET' => 'utf8',      // 数据库编码默认采用utf8
+	'SHOW_PAGE_TRACE' =>true, // 显示页面Trace信息
+); 
+?>
